d8b6971aee08762859cd8e4a4451d0c0fa44e7ed
zyr-ux <siddharthasxi2003@gmail.com>
2025-01-30 21:52:39 +0530

    fixed some padding related issues

--- app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
@@ -44,7 +44,6 @@ class MainActivity : AppCompatActivity()
         lifecycleScope.launch {
             happyPlacesdao.fetchAllHappyPlaces().collect{ list ->
                 val list=ArrayList(list)
-
                 setupDataintoRV(list)
             }
         }
